db3b3e211583c39113d35cd71a5fecbfa58e6f71
loki0b <social@henriquegarcialeite.com>
2025-06-13 23:25:57 -0300

    improving request definition, adding getrs function

--- backend/include/request.hpp
+++ backend/include/request.hpp
@@ -2,16 +2,21 @@
 #define REQUEST_HPP
 
 #include <string>
+#include <curl/curl.h>
 #include "response.hpp"
 
 class Request {
 private:
+    CURL* handle;
     std::string url;
-    size_t writeData(char* ptr, size_t size, size_t nmemb, void* userdata);
+protected:
+    static size_t writeData(void* buffer, size_t size, size_t nmemb, void* userdata);
 public:
     Request(std::string url);
-    ~Request();
+    virtual ~Request();
     virtual Response request();
+    CURL* getHandle() const;
+    std::string getUrl() const;
 };
 
 #endif
