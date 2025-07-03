cf57be052ee3cadbff2316583f6ea87f199a7421
loki0b <social@henriquegarcialeite.com>
2025-07-03 09:19:21 -0300

    update

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,22 @@
 import express, { Request, Response } from "express";
+import { execFile } from "child_process";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 8000;
 const host = "127.0.0.1"
+const __dirname = import.meta.dirname;
 
-app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Hello, world!" });
+const binPath = path.join(__dirname, "bin", "api_handler");
+const args = ["google.com"]
+
+execFile(binPath, args, (error, stdout, stderr) => {
+    console.log(stdout);
+});
+
+app.use(express.json());
+app.post("/", (req: Request, res: Response) => {
+    console.log(req.body); // receive json, process and send to front
 });
 
 app.listen(port, () => {
