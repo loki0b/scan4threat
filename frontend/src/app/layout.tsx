22027f1c8a4c9927095a16b945ef2e0c85d035ac
loki0b <social@henriquegarcialeite.com>
2025-07-04 16:15:20 -0300

    add frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Kode_Mono } from "next/font/google";
+import "./globals.css";
+
+const kodeMono = Kode_Mono({
+  variable: "--font-kode-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "scan4threat",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${kodeMono.variable} antialiased bg-black`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
