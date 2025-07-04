e33ec84ac528d5a2c694ad08a2d08c04a00347b7
loki0b <contato@henriquegarcialeite.com>
2025-07-03 22:30:08 -0300

    help

--- backend/routes/scan.ts
+++ backend/routes/scan.ts
@@ -3,12 +3,16 @@ import { execApiHandler } from "../app/apiHandlerCpp";
 
 const scanRouter = Router();
 
-scanRouter.post("/scanUrl", (req: Request, res: Response) => {
+scanRouter.post("/", (req: Request, res: Response) => {
     const data: string = req.body;
     const url: string = data["url"];
+
+    console.log("ping");
     
     const apiResponse: string = execApiHandler(url);
     res.send(
         JSON.stringify(apiResponse)
     )
 })
+
+export default scanRouter;
