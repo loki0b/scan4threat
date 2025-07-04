453d0f4efd7c36be9b40f15b4db2fd8b74dbff41
loki0b <social@henriquegarcialeite.com>
2025-07-04 15:36:47 -0300

    improving use of apikey

--- backend/app/apiHandlerCpp.ts
+++ backend/app/apiHandlerCpp.ts
@@ -3,9 +3,10 @@ import path from "path";
 
 function execApiHandler(url: string): string {
     const binPath = path.join("/home/loki0b/eecs/scan4threat/backend/", "bin", "api_handler");
-    const args = [url]
+    const apiKey: any | undefined = process.env.API_KEY; 
+    const args = [url, apiKey]
     
-    execFile(binPath, args, (error, stdout, stderr) => {
+    execFile(binPath, args, (error: any, stdout: any, stderr: any) => {
         console.log(stdout);
     });
     
