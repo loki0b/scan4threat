ec56521a553a28400ccb3c01bda7ee77e5562d68
ennalyc <ecsl2@cin.ufpe.br>
2025-07-06 19:34:34 -0300

    wait

--- frontend/src/stores/ApiKeyStore.ts
+++ frontend/src/stores/ApiKeyStore.ts
@@ -4,17 +4,21 @@ type ApiStore = {
     apikey: string | null;
     showLoadingBar: boolean;
     response: string;
+    analisys: string;
     updateLoadingBar: (loadingBar: boolean) => void;
     updateApiKey: (myKey: string | null) => void;
     updateResponse: (resp: string) => void;
+    updateAnalisys: (analize: string) => void;
 }
 
 export const ApiKeyStore = create<ApiStore>((set) => ({
     apikey: '',
     showLoadingBar: false,
     response: '',
+    analisys: '',
     updateLoadingBar: (myLoadingBar: boolean) => set (() => ({showLoadingBar: myLoadingBar})),
     updateApiKey: (key: string | null) => set(() => ({apikey: key})),
     updateResponse: (respo) => set(() => ({response: respo})),
+    updateAnalisys: (analizing) => set(() => ({analisys: analizing}))
 
 }))
