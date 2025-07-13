083ae7339864fa710efdcacf151fbd78643b8dab
loki0b <social@henriquegarcialeite.com>
2025-07-13 13:01:44 -0300

    fix

--- backend/cpp_modules/get.cpp
+++ backend/cpp_modules/get.cpp
@@ -38,14 +38,3 @@ Response Get::init() {
     Response response(data);    
     return response;
 }
-
-CURL *hnd = curl_easy_init();
-
-//curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "GET");
-
-
-
-
-
-
-CURLcode ret = curl_easy_perform(hnd);
